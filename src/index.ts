8a6eb6b9ea9b5d372472a16ad5948e7b4b0e6dd8
Gabriela Leichtweis <gabrielaleichtweis@gmail.com>
2023-02-15 20:59:53 -0300

    Função print

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,26 @@ import {Transaction, Wallet} from "./types"
     transactions:[]
 }
 
-function addTransaction(transaction: Transaction){
-    myWallet.transactions.push(transaction)
+const myWallet2: Wallet = {
+    enable: true, 
+    transactions:[]
 }
 
-addTransaction({description:"Salário", type:"C",value:12000})
-addTransaction({description:"Lanche", type:"D", value:100})
-addTransaction({description:"PS5", type:"D", value:5000})
+function addTransaction(transaction: Transaction, wallet: Wallet){
+    wallet.transactions.push(transaction)
+}
 
-console.log(myWallet)
+function printTransactions(wallet: Wallet){
+    wallet.transactions.forEach((item) => {
+        console.log(`Transação: ${item.description} \n Tipo: ${item.type} \n Valor: R$ ${item.value} \n`);
+        
+    })
+}
+
+addTransaction({description:"Salário", type:"C",value:12000}, myWallet)
+addTransaction({description:"Lanche", type:"D", value:100}, myWallet)
+addTransaction({description:"PS5", type:"D", value:5000}, myWallet)
+addTransaction({description:"Guardar dinheiro", type:"C", value:1000}, myWallet2)
+
+printTransactions(myWallet)
+
